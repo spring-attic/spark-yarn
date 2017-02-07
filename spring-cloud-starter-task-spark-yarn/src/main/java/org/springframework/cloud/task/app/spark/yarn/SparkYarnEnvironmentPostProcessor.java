fd100ffc7c562443d37e0d35f3b1d38a76c01150
Thomas Risberg <trisberg@pivotal.io>
2017-02-07 15:17:04 -0500

    Resolving startup failure for spark-yarn-task
    - adding EnvironmentPostProcessor to turn off web environment which seems to be triggered in newer Boot version by a Spark dependency
    
    - updating to Spark 1.6.3
    
    - removing obsolete referende to white-list property class for common properties
    
    Resolves #2

---
+++ spring-cloud-starter-task-spark-yarn/src/main/java/org/springframework/cloud/task/app/spark/yarn/SparkYarnEnvironmentPostProcessor.java
@@ -0,0 +1,35 @@
+/*
+ * Copyright 2017 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.springframework.cloud.task.app.spark.yarn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.env.EnvironmentPostProcessor;
+import org.springframework.core.env.ConfigurableEnvironment;
+
+/**
+ * {@link EnvironmentPostProcessor} implementation that will disable the web environment that seems to be
+ * triggered by a Spark code dependency.
+ *
+ * @author Thomas Risberg
+ */
+public class SparkYarnEnvironmentPostProcessor implements EnvironmentPostProcessor {
+
+	@Override
+	public void postProcessEnvironment(ConfigurableEnvironment configurableEnvironment, SpringApplication springApplication) {
+		springApplication.setWebEnvironment(false);
+	}
+}
